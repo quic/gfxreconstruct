e0348ce2866f77b1ed249b543f2f0b554f3888ac
Dustin Graves <quic_dgraves@quicinc.com>
2024-07-26 15:45:05 -0600

    Keep persistent memory in table for DX11
    Leave the persistent memory allocated for the D3D11_WRITE_DISCARD and
    D3D11_WRITE_NO_OVERWRITE DX11 map modes in the memory tracking table
    until the resources are released. Adds support for updating the
    mapped memory ID and pointer stored by PageGuardManager for use with
    DX11 resources mapped with the D3D11_MAP_DISCARD flag, which can
    return a new allocation for the resource.
    - Use separate values for the memory ID written to the capture file
      and the key used with the PageGuardManager table, allowing the ID to
      be updated without needing to add/remove the entry (the ID written
      to the capture file used to be the key).
    - Add a new PageGuardManager function to update the memory ID and
      pointer associated with a Resource, used to update the Resource entry
      with the new memory pointer returned with D3D11_MAP_DISCARD.

--- framework/encode/d3d12_capture_manager.h
+++ framework/encode/d3d12_capture_manager.h
@@ -1049,10 +1049,14 @@ class D3D12CaptureManager : public ApiCaptureManager
     void                                PrePresent(IDXGISwapChain_Wrapper* wrapper);
     void                                PostPresent(IDXGISwapChain_Wrapper* wrapper, UINT flags);
     std::shared_ptr<ID3D11ResourceInfo> GetResourceInfo(ID3D11Resource_Wrapper* wrapper);
-    void                                FreeMappedResourceMemory(ID3D11Resource_Wrapper* wrapper);
-    void InitializeID3D11Texture2DInfo(ID3D11Texture2D_Wrapper* wrapper, const D3D11_TEXTURE2D_DESC* desc);
-    void AddViewResourceRef(ID3D11ViewInfo* info, ID3D11Resource* resource);
-    void ReleaseViewResourceRef(ID3D11ViewInfo* info);
+    void*
+          AllocateMappedResourceMemory(util::PageGuardManager* manager, MappedSubresource& mapped_subresource, size_t size);
+    void* GetMappedResourceMemory(util::PageGuardManager* manager, const MappedSubresource& mapped_subresource);
+    void* UpdateDiscardedResourceMemory(util::PageGuardManager* manager, const MappedSubresource& mapped_subresource);
+    void  FreeMappedResourceMemory(ID3D11Resource_Wrapper* wrapper);
+    void  InitializeID3D11Texture2DInfo(ID3D11Texture2D_Wrapper* wrapper, const D3D11_TEXTURE2D_DESC* desc);
+    void  AddViewResourceRef(ID3D11ViewInfo* info, ID3D11Resource* resource);
+    void  ReleaseViewResourceRef(ID3D11ViewInfo* info);
     static D3D12CaptureManager*       singleton_;
     std::set<ID3D12Resource_Wrapper*> mapped_resources_; ///< Track mapped resources for unassisted tracking mode.
     DxgiDispatchTable  dxgi_dispatch_table_;  ///< DXGI dispatch table for functions retrieved from the DXGI DLL.
